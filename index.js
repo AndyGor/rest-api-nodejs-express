1dec256a3c19f1cc0a6e56ad7b22650c3e2fed18
Goretskyi, Andriy (DE - External) <GOREAND@HILTI.com>
2025-07-17 17:25:24 +0200

    add crud operation

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/restRoutes';
 
 const app = express();
 const PORT = 3050;
+
+// Mongo DB connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/simpledb');
+
+// body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
